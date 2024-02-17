fa74dba40a4257845dc6ffe59e769d77cf40b5dd
Kastet6398 <konstantin.nezhalsky@gmail.com>
2024-02-17 11:13:43 +0200

    3

--- BonePro/settings.py
+++ BonePro/settings.py
@@ -149,15 +149,15 @@ USE_I18N = True
 
 # USE_TZ = True
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 # STATIC_ROOT = BASE_DIR / 'static'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
