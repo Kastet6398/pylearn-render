b20b420f429972939fc14c1837a41ad972fe72d9
Kastet6398 <konstantin.nezhalsky@gmail.com>
2024-02-20 11:03:16 +0200

    8

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,22 +1,27 @@
-import math
 import re
+import time
 
 import sympy
-import time
 
 
 def calculate(expr: str):
     expr = expr.lower()
-    while re.findall(r"\)([\d]+)", expr) or re.findall(r"(?<!\w)j(?!\w)", expr) \
-            or re.findall(r"([\d)])i", expr) or re.findall(r"([\w)])\(", expr) \
-            or re.findall(r"(?<!\w)i(?!\w)", expr) or re.findall(r"(?<!\w)pi(?!\w)", expr) \
-            or re.findall(r"(?<!\w)pi(?!\w)", expr) or re.findall(r"(?<!\w)e(?!\w)", expr) \
+    while re.findall(r"([a-zA-Z)]+(?<!sin)(?<!cos)(?<!tan)(?<!acos)(?<!asin)(?<!atan)(?<!cot)(?<!acot)(?<!sinc)("
+                     r"?<!sinh)(?<!asinh)(?<!acosh)(?<!cosh)(?<!tanh)(?<!cotanh)(?<!atanh)(?<!acoth))\(",
+                     expr) or re.findall(r"\)([\d]+)", expr) or re.findall(r"(?<!\w)j(?!\w)", expr) \
+            or re.findall(r"([\d)])i", expr) \
+            or re.findall(r"(?<!\w)i(?!\w)", expr) \
             or re.findall(r"([\d)])([a-ik-z]+)", expr):
-        expr = re.sub(r"([\w)])\(", "\\1*(", expr)
-        expr = re.sub(r"([\d)])i", "\\1*1j", expr)
+        expr = re.sub(r"([a-zA-Z)]+(?<!sin)(?<!cos)(?<!tan)(?<!acos)(?<!asin)(?<!atan)(?<!cot)(?<!acot)(?<!sinc)("
+                      r"?<!sinh)(?<!asinh)(?<!acosh)(?<!cosh)(?<!tanh)(?<!cotanh)(?<!atanh)(?<!acoth))\(", "\\1*(",
+                      expr)
+        print(expr)
+        expr = re.sub(r"([\d)])i(?!\w)", "\\1*1j", expr)
+        print(expr)
         expr = re.sub(r"([\d)])\(", "\\1(", expr)
         print(expr)
-        expr = re.sub(r"([\d)])([a-ik-z]+)", "\\1*\\2", expr)
+        expr = re.sub(r"([\d)])(?!(sin|cos|tan|acos|asin|atan|cot|acot|sinc|sinh|asinh|acosh|cosh|tanh|cotanh|atanh"
+                      r"|acoth)\b)([a-ik-z]+)", "\\1*\\3", expr)
         print(expr)
         expr = re.sub(r"\)([\d]+)", ")*\\1", expr)
         print(expr)
@@ -24,10 +29,9 @@ def calculate(expr: str):
         print(expr)
         expr = re.sub(r"(?<!\w)i(?!\w)", "1j", expr)
         print(expr)
-        expr = re.sub(r"(?<!\w)pi(?!\w)", f"{math.pi}", expr)
-        print(expr)
-        expr = re.sub(r"(?<!\w)e(?!\w)", f"{math.e}", expr)
+        expr = re.sub(r"([\d)])([a-ik-z]+)", "\\1*\\2", expr)
         print(expr)
         print(2222222)
-    result = sympy.sympify(expr)
+    result = sympy.simplify(expr).evalf()
+    print(result)
     return str(result)
