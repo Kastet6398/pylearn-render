d893e6e91b154c72fe86c662232334d968759ea9
Kastet6398 <konstantin.nezhalsky@gmail.com>
2024-02-20 15:24:44 +0200

    3

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,3 +1,4 @@
+import math
 import re
 import time
 
@@ -6,29 +7,24 @@ import sympy
 
 def calculate(expr: str):
     expr = expr.lower()
-    while re.findall(r"([a-zA-Z)]+(?<!sin)(?<!cos)(?<!tan)(?<!acos)(?<!asin)(?<!atan)(?<!cot)(?<!acot)(?<!sinc)("
-                     r"?<!sinh)(?<!asinh)(?<!acosh)(?<!abs)(?<!cosh)(?<!tanh)(?<!cotanh)(?<!atanh)(?<!acoth))\(",
-                     expr) or re.findall(r"\)([\d]+)", expr) or re.findall(r"(?<!\w)j(?!\w)", expr) \
+    while re.findall(r"\)([\d]+)", expr) or re.findall(r"(?<!\w)j(?!\w)", expr) \
             or re.findall(r"([\d)])i(?!\w)", expr) \
-            or re.findall(r"(?<!\w)i(?!\w)", expr) or re.findall(r"(?<!\w)c(?!\w)", expr) \
+            or re.findall(r"(?<!\w)i(?!\w)", expr) or re.findall(r"(?<!\w)c(?!\w)", expr) or re.findall(r"(?<!\w)e(?!\w)", expr) \
             or re.findall(r"([\d)])([a-ik-z]+)", expr):
-        expr = re.sub(r"([a-zA-Z)]+(?<!sin)(?<!abs)(?<!cos)(?<!tan)(?<!acos)(?<!asin)(?<!atan)(?<!cot)(?<!acot)("
-                      r"?<!sinc)("
-                      r"?<!sinh)(?<!asinh)(?<!acosh)(?<!cosh)(?<!tanh)(?<!cotanh)(?<!atanh)(?<!acoth))\(", "\\1*(",
-                      expr)
-        print(expr)
+
         expr = re.sub(r"([\d)])i(?!\w)", "\\1*1j", expr)
         print(expr)
         expr = re.sub(r"([\d)])\(", "\\1(", expr)
         print(expr)
-        expr = re.sub(r"([\d)])(?!(sin|cos|abs|tan|acos|asin|atan|cot|acot|sinc|sinh|asinh|acosh|cosh|tanh|cotanh|atanh"
-                      r"|acoth)\b)([a-ik-z]+)", "\\1*\\3", expr)
+        expr = re.sub(r"([\d)])([a-ik-z]+)", "\\1*\\2", expr)
         print(expr)
         expr = re.sub(r"\)([\d]+)", ")*\\1", expr)
         print(expr)
         expr = re.sub(r"(?<!\w)j(?!\w)", "1j", expr)
         print(expr)
         expr = re.sub(r"(?<!\w)c(?!\w)", "299792458", expr)
+        print(expr)
+        expr = re.sub(r"(?<!\w)e(?!\w)", str(math.e), expr)
         print(expr)
         expr = re.sub(r"(?<!\w)i(?!\w)", "1j", expr)
         print(expr)
